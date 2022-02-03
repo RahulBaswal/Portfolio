c71be77dd4c8e915f6c3cb3bfc0f5f6a3e98ba5d
Rahul Baswal <rb.rahulbaswal@gmail.com>
2022-02-03 17:40:38 +0530

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,7 @@
+"use strict";
+
+//Enable tooltips everywhere
+var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+  return new bootstrap.Tooltip(tooltipTriggerEl)
+});
